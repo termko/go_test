8a108c0c90d1a7c6b744d1b0708104cab2d00f31
termko <52379073+termko@users.noreply.github.com>
2020-11-11 17:33:08 +0300

    working

--- test.go
+++ test.go
@@ -8,7 +8,6 @@ import (
 
 func main() {
 	var n int
-	var flag bool = false
 	var res int = 0
 
 	if len(os.Args) != 2 {
@@ -20,19 +19,10 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	// fmt.Println(n, reflect.TypeOf(n))
-	if n < 0 {
-		flag = true
-		n = -n
-	}
-	for n > 0 {
-		if n&1 == 1 {
+	for i := 0; i < 64; i++ {
+		if (n>>i)&1 == 1 {
 			res++
 		}
-		n >>= 1
-	}
-	if flag == true {
-		res = 64 - res + 1
 	}
 	fmt.Println(res)
 }
